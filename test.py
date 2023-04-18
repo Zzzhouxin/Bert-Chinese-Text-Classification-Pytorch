5948576972b534a963e7de44361f4d45db72ca27
zhouxin <zx635029155@163.com>
2023-04-19 00:38:55 +0800

    bert-update

---
+++ test.py
@@ -0,0 +1,24 @@
+from transformers import BertModel, BertTokenizer
+
+pretrain_path = './bert_pretrain'
+
+model = BertModel.from_pretrained(pretrain_path)
+tokenizer = BertTokenizer.from_pretrained(pretrain_path)
+
+print(f'vocab size:{tokenizer.vocab_size}')
+print(tokenizer.encode('[PAD]'))
+print(tokenizer.encode('[SEP]'))
+
+print(tokenizer.tokenize('lighttpd'))
+print(tokenizer.encode('lighttpd'))
+
+print(tokenizer.tokenize('lighttpd'))
+
+new_tokens = ['lighttpd', 'hospitalization']
+num_added_toks = tokenizer.add_tokens(new_tokens)  # 返回一个数，表示加入的新词数量，在这里是2
+
+# 关键步骤，resize_token_embeddings输入的参数是tokenizer的新长度
+model.resize_token_embeddings(len(tokenizer))
+
+print(tokenizer.tokenize('lighttpd'))
+tokenizer.save_pretrained("./bert_pretrain")
