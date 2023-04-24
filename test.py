cb3474b8a47c682f05164ac8bd61c65cedda562e
zhouxin <zx635029155@163.com>
2023-04-24 20:35:42 +0800

    update

--- test.py
+++ test.py
@@ -13,12 +13,13 @@ print(tokenizer.tokenize('lighttpd'))
 print(tokenizer.encode('lighttpd'))
 
 print(tokenizer.tokenize('lighttpd'))
+print(tokenizer.tokenize('A  �A  .�A  P�A  u�A     \�A   �          �   ��� BaseException'))
 
-new_tokens = ['lighttpd', 'hospitalization']
-num_added_toks = tokenizer.add_tokens(new_tokens)  # 返回一个数，表示加入的新词数量，在这里是2
-
-# 关键步骤，resize_token_embeddings输入的参数是tokenizer的新长度
-model.resize_token_embeddings(len(tokenizer))
-
-print(tokenizer.tokenize('lighttpd'))
-tokenizer.save_pretrained("./bert_pretrain")
+# new_tokens = ['lighttpd', 'hospitalization']
+# num_added_toks = tokenizer.add_tokens(new_tokens)  # 返回一个数，表示加入的新词数量，在这里是2
+#
+# # 关键步骤，resize_token_embeddings输入的参数是tokenizer的新长度
+# model.resize_token_embeddings(len(tokenizer))
+#
+# print(tokenizer.tokenize('lighttpd'))
+# tokenizer.save_pretrained("./bert_pretrain")
